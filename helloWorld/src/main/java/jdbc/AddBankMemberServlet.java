e8e21d4daa63783a64a7ebbf45c72591969387e4
admin <admin@YD-12>
2022-08-22 17:55:18 +0900

    html에서 DAO insert해봄

---
+++ helloWorld/src/main/java/jdbc/AddBankMemberServlet.java
@@ -0,0 +1,43 @@
+package jdbc;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AddBankMemberServlet")
+public class AddBankMemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AddBankMemberServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// a태그의 파라미터 정보 읽어들이도록.
+		// id=user1&pw=1234&name=김사용&role=0
+		request.setCharacterEncoding("utf-8");
+		
+		String id = request.getParameter("id");
+		String pw  = request.getParameter("pw");
+		String nm = request.getParameter("name");
+		String rl = request.getParameter("role");
+		
+		Member member = new Member();
+		member.setMemberId(id);
+		member.setMemberPw(pw);
+		member.setMemberName(nm);
+		member.setRole(rl);
+		
+		MemberManage.getInstance().registCustomer(member);
+		
+		response.sendRedirect("index.html");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}

78e53304cbc2018dcfd1ce7f09a221d63fb14fa9
전성하 <ss03050@naver.com>
2022-08-22 22:04:33 +0900

    오늘 배운 html이랑 servlet 복습

---
+++ helloWorldReview/src/main/java/jdbc/AddBankMemberServlet.java
@@ -0,0 +1,43 @@
+package jdbc;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AddBankMemberServlet")
+public class AddBankMemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AddBankMemberServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		
+		String id = request.getParameter("id");
+		String nm = request.getParameter("name");
+		String pw = request.getParameter("pw");
+		String rl = request.getParameter("role");
+		
+		Member member = new Member();
+		member.setMemberId(id);
+		member.setMemberName(nm);
+		member.setMemberPw(pw);
+		member.setRole(rl);
+		
+		MemberManage.getInstance().registCustomer(member);
+		
+		response.sendRedirect("index.html");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

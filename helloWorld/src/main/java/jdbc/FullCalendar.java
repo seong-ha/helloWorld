8ae7ea5948fde03bd012cd4b9d50a57ac31ee047
admin <admin@YD-12>
2022-09-05 18:38:34 +0900

    0905 chart, calendar, fetch

---
+++ helloWorld/src/main/java/jdbc/FullCalendar.java
@@ -0,0 +1,32 @@
+package jdbc;
+
+public class FullCalendar {
+	private String title;
+	private String startDate;
+	private String endDate;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+}
--- helloWorld/src/main/java/jdbc/MemberManage.java
+++ helloWorld/src/main/java/jdbc/MemberManage.java
@@ -4,84 +4,150 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MemberManage extends DAO {
-	
-	//싱글톤
+
+	// 싱글톤
 	private static MemberManage mm = new MemberManage();
-	
+
 	private MemberManage() {
-		
+
 	}
-	
+
 	public static MemberManage getInstance() {
 		return mm;
 	}
+
+	// 풀캘린더 관련(from)
+	public List<FullCalendar> scheduleList() {
+		List<FullCalendar> list = new ArrayList<>();
+		
+		try {
+			conn();
+			String sql = "select * from my_calendar";
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				FullCalendar cal = new FullCalendar();
+				cal.setTitle(rs.getString("title"));
+				cal.setStartDate(rs.getString("start_date"));
+				cal.setEndDate(rs.getString("end_date"));
+				list.add(cal);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		
+		return list;
+	}
+
+	// 풀캘린더 관련(to)
+	public boolean insertCalendar(FullCalendar full) {
+		int result = 0;
+		try {
+			conn();
+			String sql = "insert into my_calendar values (?,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, full.getTitle());
+			pstmt.setString(2, full.getStartDate());
+			pstmt.setString(3, full.getEndDate());
+
+			result = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return (result > 0) ? true : false;
+	}
 	
-	//로그인 메소드
+	// 풀캘린더 관련(delete)
+	public boolean deleteCalendar(FullCalendar full) {
+		int result = 0;
+		try {
+			conn();
+			String sql = "delete from my_calendar where title = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, full.getTitle());
+			
+			result = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return (result > 0) ? true : false;
+	}
+	
+	// 로그인 메소드
 	public Member loginInfo(String id) {
 		Member member = null;
 		try {
 			conn();
-			//id -> PK
+			// id -> PK
 			// 1 row
-			// id / pw / ..../ 
+			// id / pw / ..../
 			String sql = "select * from bankmember where member_id = ?";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
-			
+
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				member = new Member();
-				//MemberManage -> 로그인 메소드
-				//setAccountId -> setMemberId
+				// MemberManage -> 로그인 메소드
+				// setAccountId -> setMemberId
 				member.setMemberId(rs.getString("member_id"));
 				member.setMemberPw(rs.getString("member_pw"));
 				member.setMemberName(rs.getString("member_name"));
 				member.setRole(rs.getString("role"));
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			disconnect();
 		}
 		return member;
 	}
-	
-	//고객 등록 메소드
+
+	// 고객 등록 메소드
 	public int registCustomer(Member member) {
 		int result = 0;
 		try {
 			conn();
-			String sql = "insert into bankMember (member_id, member_pw, "
-					+ "member_name, role) values (?,?,?,?)";
+			String sql = "insert into bankMember (member_id, member_pw, " + "member_name, role) values (?,?,?,?)";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, member.getMemberId());
 			pstmt.setString(2, member.getMemberPw());
 			pstmt.setString(3, member.getMemberName());
 			pstmt.setString(4, member.getRole());
-			
+
 			result = pstmt.executeUpdate();
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			disconnect();
 		}
 		return result;
 	}
-	
+
 	// 전체 고객 반환 메서드
 	public List<Member> getMembers() {
 		List<Member> list = new ArrayList<>();
-		
+
 		try {
 			conn();
 			String sql = "select * from bankmember";
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(sql);
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				Member member = new Member();
 				member.setMemberId(rs.getString("member_id"));
 				member.setMemberPw(rs.getString("member_pw"));
@@ -89,35 +155,35 @@ public class MemberManage extends DAO {
 				member.setRole(rs.getString("role"));
 				list.add(member);
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			disconnect();
 		}
-		
+
 		return list;
 	}
 
 	// 아이디를 기준으로 삭제처리 후, 정상처리되면 true or false반환.
 	public boolean delMember(String id) {
-		
+
 		try {
 			conn();
 			String sql = "delete bankmember where member_id = ?";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
-			
+
 			int result = pstmt.executeUpdate();
 			if (result > 0) {
 				return true; // 정상 처리
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			disconnect();
 		}
-		
+
 		return false; // 비정상 처리
 	}
 
@@ -132,15 +198,15 @@ public class MemberManage extends DAO {
 			pstmt.setString(2, member.getMemberName());
 			pstmt.setString(3, member.getRole());
 			pstmt.setString(4, member.getMemberId());
-			
+
 			result = pstmt.executeUpdate();
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			disconnect();
 		}
 		return (result > 0) ? true : false;
 	}
-	
+
 }
